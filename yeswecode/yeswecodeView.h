4204e402deb95ec9c24cb83b81b1ee49a8489f13
Britt Selvitelle <brittspace@gmail.com>
2012-09-29 22:03:11 -0500

    Initial Commit

---
+++ yeswecode/yeswecodeView.h
@@ -0,0 +1,13 @@
+//
+//  yeswecodeView.h
+//  yeswecode
+//
+//  Created by Britt Selvitelle on 9/29/12.
+//  Copyright (c) 2012 Britt Selvitelle. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface yeswecodeView : ScreenSaverView
+
+@end
---
+++ yeswecode/yeswecodeView.m
@@ -0,0 +1,52 @@
+//
+//  yeswecodeView.m
+//  yeswecode
+//
+//  Created by Britt Selvitelle on 9/29/12.
+//  Copyright (c) 2012 Britt Selvitelle. All rights reserved.
+//
+
+#import "yeswecodeView.h"
+
+@implementation yeswecodeView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
